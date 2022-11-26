84cacf77b64f6029f012b468befbb1d7baeb5877
milk717 <robolindasoo@gmail.com>
2022-11-26 22:14:01 +0900

    feat: 구글 어시스턴트와 연동함
    아,,, 이렇게 쓰는거였구나,, 이렇게나 간단한걸

--- app/src/main/java/com/strongcom/google_assistant_test/MainActivity.kt
+++ app/src/main/java/com/strongcom/google_assistant_test/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 
+private const val GET_THING = "name"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,10 +14,11 @@ class MainActivity : AppCompatActivity() {
 
         val action = intent?.action
         val data = intent?.data
+        val action2 = intent?.extras?.getString(GET_THING)
 
-        Log.d("asdhjfhasdkljfhsa", (action ?: "action 없음"))
-        Log.d("asdhjfhasdkljfhsa", "data == "+data.toString())
+        Log.d("asdhjfhasdkljfhsa", "get thing "+(action2 ?: "문자"))
+//        Log.d("asdhjfhasdkljfhsa", (action ?: "action 없음"))
+//        Log.d("asdhjfhasdkljfhsa", "data == " + data.toString())
 
-        Toast.makeText(applicationContext, "$action",Toast.LENGTH_SHORT).show()
     }
 }
